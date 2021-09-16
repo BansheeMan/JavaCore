cb761a624096e4074e9395643301bdfd251dc4a5
Banshee <klindyuk98@gmail.com>
2021-09-16 17:58:26 +0300

    addHW11

---
+++ lesson11/task3/Orange.java
@@ -0,0 +1,12 @@
+package lesson11.task3;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5F);
+    }
+
+    @Override
+    public String toString() {
+        return "апельсины";
+    }
+}
